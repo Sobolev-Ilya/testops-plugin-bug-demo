ce76efaf9b17283efd3922cef4cffcc8a6589fff
Ilya <isobolev@detmir.ru>
2021-12-02 16:23:22 +0300

    init

---
+++ src/test/java/detmir/IncorrectAllureIdAssignTests.java
@@ -0,0 +1,16 @@
+package detmir;
+
+import org.testng.annotations.Test;
+
+public class IncorrectAllureIdAssignTests {
+
+    @Test
+    public void requestOrdersByPhone(){
+
+    }
+
+    @Test
+    public void requestOrders(){
+
+    }
+}
